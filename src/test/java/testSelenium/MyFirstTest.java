2f3faacaa3c0c9ab59c390cf45f5eeadfcc8a1ed
MKP9 <michael.keane.peterson@live.com>
2017-11-08 18:29:07 -0800

    Changed root folder name to Selenium_Web_Driver_Practice

--- src/test/java/testSelenium/MyFirstTest.java
+++ src/test/java/testSelenium/MyFirstTest.java
@@ -5,7 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class MyFirstTest {
 
